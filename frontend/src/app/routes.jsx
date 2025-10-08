00c700c6869dc137cdf9b2d0e71fd276ca6e33ac
Deanna <e1157414@u.nus.edu>
2025-10-08 17:48:15 +0800

    Add Forgot Password pages

--- frontend/src/app/routes.jsx
+++ frontend/src/app/routes.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Landing from "../features/auth/pages/Landing";
 import Login from "../features/auth/pages/Login";
 import Signup from "../features/auth/pages/Signup";
+import Verification from "../features/auth/pages/forgotPassword/Verification";
+import ForgotPassword from "../features/auth/pages/forgotPassword/ForgotPassword";
 
 export default function RoutesDef() {
   return (
@@ -9,6 +11,8 @@ export default function RoutesDef() {
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/forgotpassword-verification" element={<Verification/>}/>
+      <Route path="/forgotpassword" element={<ForgotPassword/>}/>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
