839f2f3aed2314d8dc78ef3b1a176da16fd1a19b
Deanna <e1157414@u.nus.edu>
2025-10-22 19:13:10 +0800

    Fix 'End session' button bug in Room.jsx

--- frontend/src/features/session/pages/Room.jsx
+++ frontend/src/features/session/pages/Room.jsx
@@ -314,12 +314,12 @@ function hasCycle(head){
               <div className="relative mb-2">
               {/* connection status */}
               {!socketReady && (
-                <p className="text-sm text-gray-500 mb-2">
+                <p className="text-sm text-gray-500 mb-8">
                   Connecting to collaboration service...
                 </p>
               )}
               {socketReady && (
-                <p className="text-sm text-green-600 mb-2">
+                <p className="text-sm text-green-600 mb-8">
                   Connected as <b>{username}</b>
                 </p>
               )}
@@ -327,7 +327,7 @@ function hasCycle(head){
 
               <button
                 onClick={() => setShowLeaveConfirm(true)}
-                className="absolute right-4 top-1/2 -translate-y-1/2 px-3 py-1 rounded-md text-sm font-medium text-white bg-red-700 hover:bg-red-800 transition"
+                className="right-4 top-1/2 -translate-y-1/2 px-3 py-1 rounded-md text-sm font-medium text-white bg-red-700 hover:bg-red-800 transition"
               >
                 End Session
               </button>
